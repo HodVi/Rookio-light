86fdc93e3405fadf4be28b5b1f2ff14de591ebb9
HodVi <hodina.viktor@gmail.com>
2023-07-20 22:19:01 +0200

    added - tag implementation into "Add new Room", up to 10 tags per room -> validation of uniqueness, re-rendering

--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,4 @@
+import itertools
 
 from django.contrib import messages
 
@@ -8,9 +9,9 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from django.shortcuts import render, redirect
 
-from viewer.forms import CustomUserCreationForm, RoomForm
+from viewer.forms import CustomUserCreationForm, RoomForm, TagForm
 
-from viewer.models import Room, PersonOwner
+from viewer.models import Room, PersonOwner, Tag
 
 
 def register(request):
@@ -56,12 +57,30 @@ def welcome(request):
     return render(request, "welcome.html")
 
 
+# TODO ak existuje tag tak umozni odoslat formular ALE priradi existujuci tag k novovytvaranej roomke
+
+# TODO BUG, ASI uz fixnute otestovat moznosti a ci sa nezmenila fukcionalita inych validacii tagov -
+#  zmazanim tag fieldov napr zo stredu a naslednym pridanim,
+#  vzniknu duplikaty Tag nameov a potom data z jedneho z duplikatov nezapise do DB
 @login_required
 def add_room(request):
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            new_room = form.save(commit=False)
+        room_form = RoomForm(request.POST)
+        # total_tags = int(request.POST.get('total-tags', 0))
+
+        tag_names = [value for key, value in request.POST.items() if key.startswith('tag-')]
+
+        # check if it has any duplicates by comparing its length with the length of its set
+        if len(tag_names) != len(set(tag_names)):
+            messages.error(request, 'Tag names must be unique.')
+            return render(request, 'add_room.html', {'room_form': room_form, 'tag_names': tag_names})
+
+        tag_forms = [TagForm({'name': tag_name}) for tag_name in tag_names]
+
+        all_forms_valid = room_form.is_valid() and all(tag_form.is_valid() for tag_form in tag_forms)
+
+        if all_forms_valid:
+            new_room = room_form.save(commit=False)
             person_owner, created = PersonOwner.objects.get_or_create(user_person_owner=request.user, defaults={
                 'permission': PersonOwner.PERMISSION_CHOICES[0][0]})
             if created:
@@ -69,16 +88,24 @@ def add_room(request):
                 person_owner.save()
             new_room.save()  # First save the room !!
             new_room.owners.add(person_owner)  # Then add the owner
-            messages.success(request, "Room created successfully!")
 
+            for tag_form in tag_forms:
+                tag, created = Tag.objects.get_or_create(name=tag_form.cleaned_data['name'])
+                new_room.tags.add(tag)  # Add the tag to the room
+
+            messages.success(request, "Room created successfully!")
             return redirect('welcome')  # TODO redirect to "Show Rooms" page
         else:
-            for field in form:
+            # iterate over all fields in all forms (the room form and all tag forms) in a single loop
+            # each TagForm instance in the tag_forms list is passed as a separate argument
+            for field in itertools.chain(room_form, *tag_forms):
                 for error in field.errors:
                     messages.error(request, f"{field.label}: {error}")
+            return render(request, 'add_room.html', {'room_form': room_form, 'tag_names': tag_names})
     else:
-        form = RoomForm()
-    return render(request, 'add_room.html', {'form': form})
+        room_form = RoomForm()
+        tag_names = ['']
+    return render(request, 'add_room.html', {'room_form': room_form, 'tag_names': tag_names})
 
 
 def rooms_overview(request):
